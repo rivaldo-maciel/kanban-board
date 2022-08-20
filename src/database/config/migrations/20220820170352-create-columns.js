4a239c5756a84afe8759a2901ace4be873c25b50
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-08-20 14:05:57 -0300

    feat: create columns migration

--- src/database/config/migrations/20220820170352-create-columns.js
+++ src/database/config/migrations/20220820170352-create-columns.js
@@ -2,14 +2,16 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('users_boards', {
-      userId: {
+    await queryInterface.createTable('columns', {
+      id: {
         allowNull: false,
-        type: Sequelize.INTEGER,
-        references: { model: 'users', key: 'id'},
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
-        field: 'user_id'
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
       },
       boardId: {
         allowNull: false,
@@ -23,6 +25,6 @@ module.exports = {
   },
 
   async down (queryInterface, _Sequelize) {
-    await queryInterface.dropTable('users_boards');
+    await queryInterface.dropTable('columns');
   }
 };
