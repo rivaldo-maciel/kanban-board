eb93720ae96d9c83d719f74699a9fcd0cd9dc51c
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-08-21 22:28:46 -0300

    feat: add environment vars on db config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,17 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import 'dotenv/config';
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "1234",
-    database: "KANBAN",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
-    entities: ["./src/database/models/**/*.ts"],
-    migrations: ["./src/database/migration/*.ts"],
+    entities: [".database/models/**/*.ts"],
+    migrations: [".database/migrations/*.ts"],
     subscribers: [],
 })
