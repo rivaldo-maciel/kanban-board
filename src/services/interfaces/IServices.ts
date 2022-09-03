367f41448a10247538e4c1b77567a17420ecfdf2
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-09-03 12:27:06 -0300

    create generic controllers

---
+++ src/services/interfaces/IServices.ts
@@ -0,0 +1,16 @@
+import { UpdateResult, DeleteResult } from "typeorm"
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity"
+
+interface IServices<T> {
+  create(entity: T): Promise<T>
+
+  getAll(): Promise<T[]>
+
+  getOne(id: number): Promise<T>
+
+  update(alteration: QueryDeepPartialEntity<T>, id: number): Promise<UpdateResult>
+
+  remove(id: number): Promise<DeleteResult>
+}
+
+export default IServices;
