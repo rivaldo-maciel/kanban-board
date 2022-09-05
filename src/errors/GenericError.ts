ce98b3c8fbc3b4f65004a00579808c9e1fbba83d
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-09-05 11:16:42 -0300

    refactor: change typescript target to es6

--- src/errors/GenericError.ts
+++ src/errors/GenericError.ts
@@ -2,11 +2,6 @@ abstract class GenericError extends Error {
   protected _status: number;
   protected _message: string;
 
-  constructor() {
-    super();
-    Object.setPrototypeOf(this, GenericError.prototype);
-  }
-
   get status() {
     return this._status;
   }
